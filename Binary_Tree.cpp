a0268ba40a5d5491f0e8ca43ef25463fb10f85a2
Sky_Scraper <moinsoheb02@gmail.com>
2019-01-11 22:53:38 +0530

    Create Binary_Tree.cpp

---
+++ Binary_Tree.cpp
@@ -0,0 +1,16 @@
+pair<int,int> hd(node* nn)
+{
+    if(nn == NULL)
+    return {0,0};//{h,d}
+    pair<int,int>ans;
+    pair<int,int>left_data = hd(nn->l);
+    pair<int,int>right_data = hd(nn->r);
+    int lh,rh,ld,rd,height,diameter;
+    lh = left_data.X;
+    rh = right_data.X;
+    ld = left_data.Y;
+    rd = right_data.Y;
+    height = 1 + max(lh,rh);
+    diameter = max( lh+rh , max(ld,rd) );
+    return {height,diameter};
+}
